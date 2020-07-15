b64b2910f4d2d5edbe5c8ac97afa774149a5d63d
arsvechkarev <arsvechkarev@gmail.com>
2020-07-15 08:16:43 +0300

    Added tests to BinaryHeap

--- src/main/java/trees/BinaryHeap.kt
+++ src/main/java/trees/BinaryHeap.kt
@@ -2,7 +2,6 @@ package trees
 
 import utils.Assert.assertThat
 
-
 class BinaryHeap<E : Comparable<E>> {
   
   private val heap = ArrayList<E>()
@@ -30,9 +29,9 @@ class BinaryHeap<E : Comparable<E>> {
     val removed = heap[index]
     heap[index] = heap.last()
     heap.removeAt(heap.lastIndex)
-    
-    swim(index)
+  
     sink(index)
+    swim(index)
     
     return removed
   }
@@ -57,6 +56,13 @@ class BinaryHeap<E : Comparable<E>> {
       left = (2 * elemIndex) + 1
       right = (2 * elemIndex) + 2
       
+      if (heap.size == 2) {
+        // Only one node and its left child left
+        if (heap[left] < heap[elemIndex])
+        swap(elemIndex, left)
+        break
+      }
+      
       // Reached heap bounds, sinking finished
       if (left >= heap.size || right >= heap.size)
         break
@@ -77,9 +83,5 @@ class BinaryHeap<E : Comparable<E>> {
     val nodeJ = heap[j]
     heap[i] = nodeJ
     heap[j] = nodeI
-  }
-  
-  override fun toString(): String {
-    return heap.toString()
   }
 }
---
+++ src/test/java/trees/BinaryHeapTest.kt
@@ -0,0 +1,33 @@
+package trees
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class BinaryHeapTest {
+  
+  @Test
+  fun `Adding and polling elements`() {
+    val heap = BinaryHeap<Int>()
+    
+    assertTrue(heap.size == 0)
+    
+    heap.add(5)
+    heap.add(1)
+    heap.add(3)
+    heap.add(2)
+    heap.add(5)
+    heap.add(6)
+    heap.add(4)
+    
+    var element = heap.poll()
+   
+    while (true) {
+      if (heap.size == 0)
+        break
+      
+      val element2 = heap.poll()
+      assertTrue(element <= element2)
+      element = element2
+    }
+  }
+}
