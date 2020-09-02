956a81139ba2d90c26aab36acb51bc8a73b09900
arsvechkarev <arsvechkarev@gmail.com>
2020-09-02 07:17:27 +0300

    Added BreadthFirstSearch.kt

---
+++ java_kotlin/main/java/graphs/BreadthFirstSearch.kt
@@ -0,0 +1,30 @@
+package graphs
+
+import datastructures.lists.DoublyLinkedList
+
+fun breadthFirstSearch(
+  graph: Map<String, Map<String, Int>>,
+  startNode: String,
+  onNodeVisited: (String, Map<String, Int>) -> Unit = { _, _ -> }
+) {
+  val queue = DoublyLinkedList<String>()
+  val visited = HashSet<String>()
+  
+  queue.add(startNode)
+  visited.add(startNode)
+  
+  while (queue.size() != 0) {
+    val current = queue[0]
+    queue.remove(0)
+    
+    onNodeVisited(current, graph.getValue(current))
+    
+    val neighbours = graph.getValue(current)
+    for (node in neighbours) {
+      if (!visited.contains(node.key)) {
+        queue.add(node.key)
+        visited.add(node.key)
+      }
+    }
+  }
+}
--- java_kotlin/main/java/graphs/GraphGenerator.kt
+++
@@ -1,4 +0,0 @@
-package graphs
-
-object GraphGenerator {
-}
