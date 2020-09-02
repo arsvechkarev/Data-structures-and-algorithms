2af1a3a866415df09c0c9852300f34c8e870c50f
arsvechkarev <arsvechkarev@gmail.com>
2020-09-02 17:20:08 +0300

    Finished Linked list implementation

--- cpp/datastructures/linked_list.cpp
+++ cpp/datastructures/linked_list.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdexcept>
+#include <string>
 
 template <class E>
 class DoublyLinkedList
@@ -20,11 +21,16 @@ private:
 
   Node *head = NULL;
   Node *tail = NULL;
+  int _size = 0;
 
 public:
-  int size = 0;
+  int size()
+  {
+    return _size;
+  }
 
-  void add(E element)
+
+  void addFirst(E element)
   {
     if (head == NULL)
     {
@@ -32,7 +38,31 @@ public:
       head = node;
       tail = node;
     }
-    else if (size == 1)
+    else if (_size == 1)
+    {
+      head = new Node(element);
+      tail->previous = head;
+      head->next = tail;
+    }
+    else
+    {
+      Node *nodeAfter = head;
+      head = new Node(element);
+      nodeAfter->previous = head;
+      head->next = nodeAfter;
+    }
+    _size++;
+  }
+
+  void addLast(E element)
+  {
+    if (head == NULL)
+    {
+      Node *node = new Node(element);
+      head = node;
+      tail = node;
+    }
+    else if (_size == 1)
     {
       tail = new Node(element);
       head->next = tail;
@@ -45,7 +75,7 @@ public:
       previousNode->next = tail;
       tail->previous = previousNode;
     }
-    size++;
+    _size++;
   }
 
   E get(int index)
@@ -54,26 +84,141 @@ public:
     {
       throw std::invalid_argument("No elements in the list");
     }
-    if (index >= size || index < 0)
+    if (index >= _size || index < 0)
     {
       throw std::invalid_argument("Index is less than 0 or greater then or equal to size");
     }
 
-    int counter = 0;
-    Node *current = head;
-    while (counter <= index - 1)
+    if (index < _size / 2)
     {
-      current = current->next;
-      counter++;
+      int counter = _size - 1;
+      Node *current = tail;
+      while (counter > index)
+      {
+        current = current->previous;
+        counter--;
+      }
+      return current->data;
     }
-    return current->data;
+    else
+    {
+      int counter = 0;
+      Node *current = head;
+      while (counter < index)
+      {
+        current = current->next;
+        counter++;
+      }
+      return current->data;
+    }
+  }
+
+  E remove(int index)
+  {
+    if (head == NULL)
+    {
+      throw std::invalid_argument("No elements in the list");
+    }
+    if (index >= _size || index < 0)
+    {
+      throw std::invalid_argument("Index is less than 0 or greater then or equal to size");
+    }
+
+    E result;
+
+    if (_size == 1)
+    {
+      result = head->data;
+      head = tail = NULL;
+      _size--;
+      return result;
+    }
+
+    if (index < _size / 2)
+    {
+      int counter = _size - 1;
+      Node *current = tail;
+      while (counter > index)
+      {
+        current = current->previous;
+        counter--;
+      }
+
+      result = current->data;
+      Node *previous = current->previous;
+      if (previous == NULL)
+      {
+        current->next->previous = NULL;
+      }
+      else
+      {
+        previous->next = current->next;
+        current->next->previous = previous;
+      }
+      current = NULL;
+    }
+    else
+    {
+      int counter = 0;
+      Node *current = head;
+      while (counter < index)
+      {
+        current = current->next;
+        counter++;
+      }
+
+      result = current->data;
+      Node *next = current->next;
+      if (next == NULL)
+      {
+        current->previous->next = NULL;
+      }
+      else
+      {
+        next->previous = current->previous;
+        current->previous->next = next;
+      }
+      current = NULL;
+    }
+    _size--;
+    return result;
   }
 };
 
+void assert(bool condition)
+{
+  if (!condition)
+  {
+    throw std::runtime_error("Error happened");
+  }
+}
+
 int main()
 {
-  DoublyLinkedList<int> *list = new DoublyLinkedList<int>;
-  std::cout << "Size = " << list->size << std::endl;
-  std::cout << "Second = " << list->get(0) << std::endl;
+  DoublyLinkedList<int> list;
+
+  assert(list.size() == 0);
+
+  list.addFirst(5);
+
+  assert(list.size() == 1);
+  assert(list.get(0) == 5);
+  assert(list.remove(0) == 5);
+  assert(list.size() == 0);
+
+  list.addFirst(2);
+  list.addFirst(1);
+  list.addLast(3);
+  list.addLast(4);
+
+  assert(list.size() == 4);
+  assert(list.get(0) == 1);
+  assert(list.get(3) == 4);
+
+  assert(list.remove(1) == 2);
+  assert(list.remove(1) == 3);
+  assert(list.remove(1) == 4);
+  assert(list.remove(0) == 1);
+  assert(list.size() == 0);
   return 0;
 }
